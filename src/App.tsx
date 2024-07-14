c4a7739bf01a8c3fefbd62883cf1358abcf2af4e
Tom Jane <kodemonki@hotmail.com>
2024-07-14 16:07:50 +0100

    hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import Header from "./components/Header";
 import Projects from "./pages/Projects";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Experience from "./pages/Experience";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,39 +1,14 @@
-import { useNavigate } from "react-router-dom";
+import { Link} from "react-router-dom";
 
 const Header = () => {
-  const navigate = useNavigate();
 
   return (
     <div className="Header">
       <p className="Header__left">Portfolio - Thomas Jane</p>
       <p className="Header__right">
-        <a
-          href=""
-          onClick={() => {
-            navigate("/projects", { replace: true });
-          }}
-        >
-          Projects
-        </a>{" "}
-        -{" "}
-        <a
-          href=""
-          onClick={() => {
-            navigate("/experience", { replace: true });
-          }}
-        >
-          Experience
-        </a>{" "}
-        -{" "}
-        <a
-          href=""
-          onClick={() => {
-            navigate("/contact", { replace: true });
-          }}
-        >
-          Contact
-        </a>{" "}
-        -{" "}
+        <Link to="/projects">projects</Link>
+        <Link to="/experience">experience</Link>
+        <Link to="/contact">contact</Link>
         <a href="/ThomasJaneCV_2024_july.pdf" download>
           Download&nbsp;CV
         </a>
