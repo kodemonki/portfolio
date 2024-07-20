f62f5c8f3d5aeb089bc46b342b3b31ab479e8169
Tom Jane <kodemonki@hotmail.com>
2024-07-20 23:22:37 +0100

    update css

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -3,21 +3,21 @@ import { ProjectData } from "../pages/Projects";
 const ProjectCard: React.FC<ProjectData> = ({ project }) => {
   const { name, description, technology, url } = project;
   return (
-    <div className="Card">       
+    <div className="Card">
       <div>
         <b>{name}</b>
       </div>
       <div>
         <i>{description}</i>
       </div>
-      <a href={url} target="_blank">
-        <img className="Card__img" src="images/static.png" />
-      </a>
+
       <div>
         <i>{technology}</i>
       </div>
+      <a href={url} target="_blank">
+        <b>Visit Link</b>
+      </a>
     </div>
-
   );
 };
 
