74e1fdc7c75692b37bda619b66bed430fa757fa2
Tom Jane <kodemonki@hotmail.com>
2024-07-21 01:20:47 +0100

    Update Starfield.ts

--- src/components/Starfield.ts
+++ src/components/Starfield.ts
@@ -31,11 +31,11 @@ export class Starfield extends Scene {
       });
     }
 
-    this.wisp = this.add.particles(1024 * 2, 768 * 2, "flares", {
+    this.wisp = this.add.particles(1024 * 2, 768, "flares", {
       frame: "white",
       color: [0xffffff, 0xffff00],
       colorEase: "quart.out",
-      lifespan: 1000,
+      lifespan: 500,
       angle: { min: 40, max: -40 },
       scale: { start: 1, end: 0, ease: "sine.in" },
       speed: { min: 150, max: 450 },
@@ -46,35 +46,30 @@ export class Starfield extends Scene {
 
   update() {
     if (this.wisp != undefined) {
-      this.wisp.x = this.input.mousePointer.x;
-      this.wisp.y = this.input.mousePointer.y;
-      const x1 = this.input.mousePointer.x;
-      const y1 = this.input.mousePointer.y;
+      this.wisp.x = this.input.x;
+      this.wisp.y = this.input.y;
+      const x1 = this.input.x;
+      const y1 = this.input.y;
       const x2 = 1024 * 2;
       const y2 = 768;
-      let angleRadians = Math.atan2(y1 - y2, x1 - x2);
+      const angleRadians = Math.atan2(y1 - y2, x1 - x2);
       this.wisp.rotation = angleRadians;
     }
     this.stars?.clear(true, true);
     if (this.points !== undefined) {
       for (let i = 0; i < this.points.length; i++) {
         const point = this.points[i];
-
         point.z -= 0.02;
-
         if (point.z <= 0) {
           point.x = Phaser.Math.Between(-25, 25);
           point.y = Phaser.Math.Between(-25, 25);
           point.z = this.maxDepth ?? 0;
         }
-
         const px =
           point.x * (128 / point.z) + Number(this.game.config.width) * 0.5;
         const py =
           point.y * (128 / point.z) + Number(this.game.config.height) * 0.5;
-
         const circle = new Phaser.Geom.Circle(px, py, (1 - point.z / 32) * 2);
-
         const graphics = this.add.graphics({
           fillStyle: { color: 0xffffff },
         });
