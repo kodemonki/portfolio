b058c3c2f54b608559472e16de8c0e237ac3f34b
Tom Jane <kodemonki@hotmail.com>
2024-07-22 13:50:32 +0100

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,25 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
+  const { pathname } = useLocation();
+  console.log(pathname);
   return (
     <div className="Header">
       <p className="Header__left">
         <Link to="/">Portfolio - Thomas Jane</Link>
       </p>
       <p className="Header__right">
-        <Link to="/projects">Projects</Link>
+        {pathname === "/projects" && (<span className="Header__selected">Projects</span>)}
+        {pathname !== "/projects" && <Link to="/projects">Projects</Link>}
         {" - "}
-        <Link to="/experience">Experience</Link>
+        {pathname === "/experience" && (<span className="Header__selected">Experience</span>)}
+        {pathname !== "/experience" && <Link to="/experience">Experience</Link>}
         {" - "}
-        <Link to="/contact">Contact</Link>
+        {pathname === "/contact" && (<span className="Header__selected">Contact</span>)}
+        {pathname !== "/contact" && <Link to="/contact">Contact</Link>}
         {" - "}
-        <Link to="/cv">CV</Link>
+        {pathname === "/cv" && (<span className="Header__selected">CV</span>)}
+        {pathname !== "/cv" && <Link to="/cv">CV</Link>}
       </p>
     </div>
   );
